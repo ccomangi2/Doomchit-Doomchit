3b90babf39087e4c902d06b877126851aaf28b1d
Lee Su Bin <ccomangi2@gmail.com>
2021-03-19 01:05:15 +0900

    프로젝트 생성

---
+++ DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.doomchit_doomchit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
