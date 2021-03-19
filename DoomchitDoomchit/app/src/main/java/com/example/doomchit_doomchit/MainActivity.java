8cb71c3af9fd13c5476bef1952fd9337d4f9c4ea
Lee Su Bin <ccomangi2@gmail.com>
2021-03-20 03:20:24 +0900

    메인 화면 프로토타입 구현 완료, 액티비티 전환 기능 구현 완료

--- DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/MainActivity.java
+++ DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.doomchit_doomchit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btn_play; // 비트 제작
+    ImageButton btn_list; // 녹음 리스트
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_play = findViewById(R.id.btn_play);
+        ImageButton btn_list = findViewById(R.id.btn_list);
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveActivity(BeatMakingActivity.class);
+            }
+        });
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveActivity(RecordListActivity.class);
+            }
+        });
+    }
+
+    // 액티비티 이동 메서드
+    public void MoveActivity(Class activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
     }
 }
