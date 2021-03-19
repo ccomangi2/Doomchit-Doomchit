5d969f4e6a719bc684db1e8a20362f327b897b87
Lee Su Bin <ccomangi2@gmail.com>
2021-03-20 04:08:11 +0900

    녹음 목록 뒤로가기 버튼, 리스트 텍스트 설정

--- DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/RecordListActivity.java
+++ DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/RecordListActivity.java
@@ -1,13 +1,32 @@
 package com.example.doomchit_doomchit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordListActivity extends AppCompatActivity {
+    ImageButton back; // 뒤로가기
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordlist);
+
+        ImageButton back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveActivity(MainActivity.class);
+            }
+        });
+    }
+
+    // 액티비티 이동 메서드
+    public void MoveActivity(Class activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
     }
 }
