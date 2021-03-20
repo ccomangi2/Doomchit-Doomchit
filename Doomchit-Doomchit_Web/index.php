d7a4c7b535467e97979cf17d8923b3a76ae0807d
맹수연 <sooyeon030908@gmail.com>
2021-03-20 13:46:50 +0900

    background image 추가

--- Doomchit-Doomchit_Web/index.php
+++ Doomchit-Doomchit_Web/index.php
@@ -2,10 +2,15 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Doomchit-Doomchit[둠칫둠칫]</title>
-</head>
-<body>
 
+<head>
+  <meta charset="utf-8">
+  <title>Doomchit-Doomchit[둠칫둠칫]</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body style="background-image: url(img/web_background.png); background-size:cover; width:100%; height:auto;">
 </body>
+<script src="js/main.js"></script>
+
 </html>
