aa66ac50cd4b214533d746477d6719be8ca4ba75
맹수연 <sooyeon030908@gmail.com>
2021-05-01 21:19:09 +0900

    css 경로 변경

--- Doomchit-Doomchit_Web/index.php
+++ Doomchit-Doomchit_Web/index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <title>Doomchit-Doomchit[둠칫둠칫]</title>
-  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body style="background-image: url(img/web_background.png); background-size:cover; width:100%; height:auto;">
