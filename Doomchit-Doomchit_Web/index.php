d345dd3873f0470228e2a2cd7cfa50eed57beaf5
맹수연 <sooyeon030908@gmail.com>
2021-05-02 00:32:12 +0900

    main.html import

--- Doomchit-Doomchit_Web/index.php
+++ Doomchit-Doomchit_Web/index.php
@@ -7,10 +7,28 @@
   <meta charset="utf-8">
   <title>Doomchit-Doomchit[둠칫둠칫]</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/index.js"></script>
 </head>
-
-<body style="background-image: url(img/web_background.png); background-size:cover; width:100%; height:auto;">
+<body>
+  <div data-include-path="main.html"></div>
+  <script>
+    window.addEventListener('load', function() {
+      var allElements = document.getElementsByTagName('*');
+      Array.prototype.forEach.call(allElements, function(el) {
+          var includePath = el.dataset.includePath;
+          if (includePath) {
+              var xhttp = new XMLHttpRequest();
+              xhttp.onreadystatechange = function () {
+                  if (this.readyState == 4 && this.status == 200) {
+                      el.outerHTML = this.responseText;
+                  }
+              };
+              xhttp.open('GET', includePath, true);
+              xhttp.send();
+          }
+      });
+  });
+  </script>
 </body>
-<script src="js/main.js"></script>
 
 </html>
