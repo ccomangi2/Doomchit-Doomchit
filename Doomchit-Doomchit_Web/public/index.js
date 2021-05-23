84294d4b8f8e892138c601ac3c413c27c6930d81
HyeongYeon Ham <guddus326@naver.com>
2021-05-24 02:31:50 +0900

    index 수정

--- Doomchit-Doomchit_Web/public/index.js
+++ Doomchit-Doomchit_Web/public/index.js
@@ -1,53 +1,25 @@
-// select BUTTOM
-const btn = document.querySelector('button')
-// ADD CLICK LISTENER TO THE BUTTON WE SELECTED
-btn.addEventListener('click',e =>{
+var firebaseConfig = {
+    apiKey: "AIzaSyDp0Ih07F0aTZltrmeWoOhQkNjcAri4CzU",
+    authDomain: "doomchit-doomchit.firebaseapp.com",
+    databaseURL: "https://doomchit-doomchit-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "doomchit-doomchit",
+    storageBucket: "doomchit-doomchit.appspot.com",
+    messagingSenderId: "898978285254",
+    appId: "1:898978285254:web:52e2e11e3770163bcf39c7",
+    measurementId: "G-B089KPEVS5"
+};
 
+firebase.initializeApp(firebaseConfig);
+var a=0
 const final=firebase.storage().ref().child(`test`)
-    
-            //find all items..
-            // Find all the prefixes and items.
-            final.listAll().then(function(res) {
-    
-                res.items.forEach(function(itemRef) {
-                    
-                    // All the items under listRef.
-                    //get the url   
-                    itemRef.getDownloadURL().then(function(url) {
-                        console.log(url);
-                       
-    
-    
-    
-                    
-                    }).catch(function(error) {
-    
-                        // A full list of error codes is available at
-                        // https://firebase.google.com/docs/storage/web/handle-errors
-                        switch (error.code) {
-                        case 'storage/object-not-found':
-                            // File doesn't exist
-                            alert("File doesn't exist");
-    
-                            break;
-    
-                        case 'storage/unauthorized':
-                            // User doesn't have permission to access the object
-                            break;
-    
-                        case 'storage/canceled':
-                            // User canceled the upload
-                            break;
-    
-                        case 'storage/unknown':
-                            // Unknown error occurred, inspect the server response
-                            break;
-                        }
-                    });
-    
-                });
-            }).catch(function(error) {
-                // Uh-oh, an error occurred!
-                console.log("Uh-oh, an error occurred!");
-            })
+final.listAll().then(function(res) {
+    res.items.forEach(function(itemRef) {
+        itemRef.getDownloadURL().then(function(url) {
+            console.log(url);
+            a+=1
+            console.log(a);
+            
         })
+    })
+})
+         
