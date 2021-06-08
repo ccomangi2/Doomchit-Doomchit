b53185effeb208fc542bb61acccb061617639e84
Lee Su Bin <ccomangi2@gmail.com>
2021-06-08 11:39:54 +0900

    비트 사운드 추가 화면

--- DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/BeatSelectActivity.java
+++ DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/BeatSelectActivity.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BeatSelectActivity extends AppCompatActivity {
@@ -34,7 +35,7 @@ public class BeatSelectActivity extends AppCompatActivity {
         original_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MoveActivity(BeatMakingActivity.class);
+                MoveActivity(BeatMakingPopActivity.class);
             }
         });
 
@@ -42,7 +43,7 @@ public class BeatSelectActivity extends AppCompatActivity {
         pop_hiphop_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MoveActivity(BeatMakingActivity.class);
+                MoveActivity(BeatMakingPopActivity.class);
             }
         });
 
@@ -50,14 +51,31 @@ public class BeatSelectActivity extends AppCompatActivity {
         classic_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MoveActivity(BeatMakingActivity.class);
+                MoveActivity(BeatMakingPopActivity.class);
             }
         });
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     // 액티비티 이동 메서드
     public void MoveActivity(Class activity) {
         Intent intent = new Intent(this, activity);
         startActivity(intent);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
--- DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/MainActivity.java
+++ DoomchitDoomchit/app/src/main/java/com/example/doomchit_doomchit/MainActivity.java
@@ -50,4 +50,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, activity);
         startActivity(intent);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
