7ba7df2de849ba6f56d0820f9c505a30690cb44f
맹수연 <sooyeon030908@gmail.com>
2021-06-22 22:42:03 +0900

    box 로직 구현 중

--- Doomchit-Doomchit_Web/public/js/list.js
+++ Doomchit-Doomchit_Web/public/js/list.js
@@ -1,51 +1,103 @@
-
-
-window.onload=function(){
-  this.getdata();
-}
-
-
-function getdata(){
-  var firebaseConfig = {
-    apiKey: 'AIzaSyDp0Ih07F0aTZltrmeWoOhQkNjcAri4CzU',
-    authDomain: 'doomchit-doomchit.firebaseapp.com',
-    databaseURL: 'https://doomchit-doomchit-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'doomchit-doomchit',
-    storageBucket: 'doomchit-doomchit.appspot.com',
-    messagingSenderId: '898978285254',
-    appId: '1:898978285254:web:52e2e11e3770163bcf39c7',
-    measurementId: 'G-B089KPEVS5'
-  };
-firebase.initializeApp(firebaseConfig);
-
-var storage=firebase.storage();
-var storageRef=storage.ref();
-var i=0;
-
-storageRef.child('wavFiles').listAll().then(function(res){
-  res.items.forEach(function(ref){
-  i++;  
-  displaylist(i,ref)
-  
-  });
-})
-function displaylist(row, audio){
-  audio.getDownloadURL().then(function(url){
-    console.log(url);
-    let htmlStr='';
-    
-htmlStr += "<div class='box'>";
-htmlStr+="<div class='song_title' id='song_title'>"+audio.name.split('_')[2]+".wav"+"</div>"
-htmlStr+="<div class='composer'>"+audio.name.split('_')[3].split('.')[0]+"</div>"
-htmlStr+="<div class='music_length'>"+audio.name.split('_')[1]+"초"+"</div>";
-htmlStr+="<div class='share' onclick='window.open('https://www.facebook.com/sharer/sharer.php');'><img src='img/web_share.png' alt='Share on Facebook'></div>";
-htmlStr+='<div class="play" onclick="window.open(url)"><img src="img/web_playsound.png" alt="play sound button"/></div>';
-htmlStr+="</div>";
-$('#container').append(htmlStr);
-
-
-  });
-
-
-}
+window.onload = function () {
+    this.getdata();
 };
+
+function getdata() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyDp0Ih07F0aTZltrmeWoOhQkNjcAri4CzU",
+        authDomain: "doomchit-doomchit.firebaseapp.com",
+        databaseURL:
+            "https://doomchit-doomchit-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "doomchit-doomchit",
+        storageBucket: "doomchit-doomchit.appspot.com",
+        messagingSenderId: "898978285254",
+        appId: "1:898978285254:web:52e2e11e3770163bcf39c7",
+        measurementId: "G-B089KPEVS5"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+    var i = 0;
+
+    storageRef
+        .child("wavFiles")
+        .listAll()
+        .then(function (res) {
+            res.items.forEach(function (ref) {
+                i++;
+                displaylist(i, ref);
+            });
+        });
+    function displaylist(row, audio) {
+        audio.getDownloadURL().then(function (url) {
+            console.log(url);
+
+            // let htmlStr = "";
+            // htmlStr += "<div class='container'>";
+
+            // htmlStr += "<div class='box'>";
+            // htmlStr +=
+            //     "<div class='song_title' id='song_title'>" +
+            //     audio.name.split("_")[2] +
+            //     ".wav" +
+            //     "</div>";
+            // htmlStr +=
+            //     "<div class='composer'>" +
+            //     audio.name.split("_")[3].split(".")[0] +
+            //     "</div>";
+            // htmlStr +=
+            //     "<div class='music_length'>" +
+            //     audio.name.split("_")[1] +
+            //     "초" +
+            //     "</div>";
+            // htmlStr +=
+            //     "<div class='share' onclick='window.open('https://www.facebook.com/sharer/sharer.php');'><img src='img/web_share.png' alt='Share on Facebook'></div>";
+            // htmlStr +=
+            //     '<div class="play" onclick="window.open(url)"><img src="img/web_playsound.png" alt="play sound button"/></div>';
+            // htmlStr += "</div>";
+            // htmlStr += "</div>";
+            // $('#container').append(htmlStr);
+
+            let count = 0;
+            let htmlStr = "";
+            // 곡 있는 개수의 /4 (반올림)  [ ex) 곡 5개면 2번 반복 ] 만큼 반복문
+            // row = stroageRef에서 넘어온 i
+            for (let cnt = 0; cnt <= Math.ceil(row / 4); cnt++) {
+                htmlStr += "<li data-thumb>";
+                htmlStr += "<div class='container'>";
+                // box 1개 추가 시 count += 1
+                // count = 4일 경우 if문 빠져나오고 cnt+=1 후 다시 li 태그 추가
+                if (count < 5) {
+                    htmlStr += "<div class='box'>";
+                    htmlStr +=
+                        "<div class='song_title' id='song_title'>" +
+                        audio.name.split("_")[2] +
+                        ".wav" +
+                        "</div>";
+                    htmlStr +=
+                        "<div class='composer'>" +
+                        audio.name.split("_")[3].split(".")[0] +
+                        "</div>";
+                    htmlStr +=
+                        "<div class='music_length'>" +
+                        audio.name.split("_")[1] +
+                        "초" +
+                        "</div>";
+                    htmlStr +=
+                        "<div class='share' onclick='window.open('https://www.facebook.com/sharer/sharer.php');'><img src='img/web_share.png' alt='Share on Facebook'></div>";
+                    htmlStr +=
+                        '<div class="play" onclick="window.open(url)"><img src="img/web_playsound.png" alt="play sound button"/></div>';
+                    htmlStr += "</div>";
+                    htmlStr += "</div>";
+
+                    count += 1;
+                }
+                htmlStr += "</div>";
+                htmlStr += "</li>";
+            }
+
+            $(ul).append(htmlStr);
+        });
+    }
+}
