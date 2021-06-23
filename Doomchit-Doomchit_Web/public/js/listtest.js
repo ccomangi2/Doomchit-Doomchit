a33c3ee8ff792f4550cc1003ef3bdcb521a1455c
HyeongYeon Ham <guddus326@naver.com>
2021-06-23 11:34:01 +0900

    Create listtest.js

---
+++ Doomchit-Doomchit_Web/public/js/listtest.js
@@ -0,0 +1,73 @@
+var arrNumber = new Array();
+window.onload=function(){
+    this.getdata();
+  }
+  
+  
+  function getdata(){
+    var firebaseConfig = {
+      apiKey: 'AIzaSyDp0Ih07F0aTZltrmeWoOhQkNjcAri4CzU',
+      authDomain: 'doomchit-doomchit.firebaseapp.com',
+      databaseURL: 'https://doomchit-doomchit-default-rtdb.asia-southeast1.firebasedatabase.app',
+      projectId: 'doomchit-doomchit',
+      storageBucket: 'doomchit-doomchit.appspot.com',
+      messagingSenderId: '898978285254',
+      appId: '1:898978285254:web:52e2e11e3770163bcf39c7',
+      measurementId: 'G-B089KPEVS5'
+    };
+  firebase.initializeApp(firebaseConfig);
+  var storage=firebase.storage();
+  var storageRef=storage.ref();
+  var i=0;
+
+  storageRef
+    .child('wavFiles')
+    .listAll()
+    .then(function(res){
+        res.items.forEach(function(ref){
+        i++;  
+        displaylist(i,ref)
+    });
+  })
+  var j=0
+
+  function displaylist(row, audio){
+    audio.getDownloadURL().then(function(url){
+        let htmlStr='';
+        arrNumber.push(url)
+        let count=0;
+        
+         
+                htmlStr += "<div class='box'>";
+                htmlStr+=
+                    "<div class='song_title' id='song_title'>"+
+                    audio.name.split('_')[2]+
+                    ".wav"+
+                    "</div>"
+                htmlStr+=
+                    "<div class='composer'>"+
+                    audio.name.split('_')[3].split('.')[0]+
+                    "</div>"
+                htmlStr+=
+                    "<div class='music_length'>"+audio.name.split('_')[1]+"초"+"</div>";
+                htmlStr+=
+                    "<div class='share' id='"+arrNumber[j]+"'><img src='img/web_share.png' alt='Share on Facebook'></div>";
+                htmlStr+="<div class='play' id='"+arrNumber[j]+"' ><img src='img/web_playsound.png' alt='play sound button'/></div>";
+                j++;
+  
+        $(".play").click(function(){
+            window.open($(this).attr('id'), '둠칫둠칫','height=100,width=180,toolbar=no');
+ 
+        });
+        $(".share").click(function(){
+            window.open("http://www.facebook.com/share.php?u="+$(this).attr('id'), '둠칫둠칫','height=250,width=300,toolbar=no');
+      
+        });
+        $('#container').append(htmlStr);
+
+        });
+        
+        }
+        
+
+        };
